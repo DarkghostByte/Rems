51eb9f7620ed322ddd0f6b4323553fe78eb6594e
DarkGhostByte <Liadeochavez@gmail.com>
2025-05-07 11:55:53 -0600

    07/05/2025

--- cli/src/views/layouts/AdminHeaderComponent.vue
+++ cli/src/views/layouts/AdminHeaderComponent.vue
@@ -6,7 +6,8 @@
           <i class="fa-solid fa-phone text-white text-xs md:text-xs lg:text-lg"></i>
         </div>
         <div class="flex justify-center items-center h-8 ">
-          <h1 class="text-white text-xs md:text-xs lg:text-lg">¿Requieres consulta? Contactanos</h1>
+          <h1 class="text-white text-[0.70rem] md:text-[0.70rem] lg:text-lg" data-i18n="contactUs">¿Requieres consulta? Contactanos
+          </h1>
         </div>
       </div>
       <div class="flex items-center space-x-2">
@@ -27,18 +28,6 @@
         </div>
       </div>
     </div>
-    <!--
-    <button
-      v-if="isMobile"
-      @click="toggleAside"
-      class="text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 mr-4"
-    >
-      <span class="sr-only">Abrir menú</span>
-      <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-      </svg>
-    </button>
-    -->
 
     <div class="flex items-center h-20 px-4 sm:px-6 lg:px-8 bgHeader">
       <router-link to="/auth/index" class="inline-flex items-center justify-center h-20 mr-4 sm:mr-6">
@@ -53,74 +42,126 @@
           </button>
           <div id="menuDropdown" class="absolute top-10 bg-white shadow-md hidden w-52 rounded-md">
             <ul class="flex flex-col items-center py-4">
-              <li><a href="#" class="text-neutral-700 text-md block py-2">Inicio</a></li>
-              <li><a href="#" class="text-neutral-700 text-md block py-2">Sobre Nosotros</a></li>
-              <li><a href="#" class="text-neutral-700 text-md block py-2">Divisiones</a></li>
-              <li><a href="#" class="text-neutral-700 text-md block py-2">Servicios</a></li>
-              <li><a href="#" class="text-neutral-700 text-md block py-2">Clientes</a></li>
-              <li><a href="#" class="text-neutral-700 text-md block py-2">Contactos</a></li>
+              <li><a href="#" class="text-neutral-700 text-md block py-2" data-i18n="home">Inicio</a></li>
+              <li><a href="#" class="text-neutral-700 text-md block py-2" data-i18n="aboutUs">Sobre Nosotros</a></li>
+              <li><a href="#" class="text-neutral-700 text-md block py-2" data-i18n="divisions">Divisiones</a></li>
+              <li><a href="#" class="text-neutral-700 text-md block py-2" data-i18n="services">Servicios</a></li>
+              <li><a href="#" class="text-neutral-700 text-md block py-2" data-i18n="clients">Clientes</a></li>
+              <li><a href="#" class="text-neutral-700 text-md block py-2" data-i18n="contacts">Contactos</a></li>
             </ul>
           </div>
         </nav>
         <nav class="hidden sm:block">
           <ul class="flex space-x-4 lg:space-x-10">
             <li>
-              <a href="#" class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
-                Inicio
-                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
+              <a href="#"
+                class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
+                <span data-i18n="home">Inicio</span>
+                <span
+                  class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
               </a>
             </li>
             <li>
-              <a href="#" class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
-                Sobre Nosotros
-                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
+              <a href="#"
+                class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
+                <span data-i18n="aboutUs">Sobre Nosotros</span>
+                <span
+                  class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
               </a>
             </li>
             <li class="relative group">
-              <a href="#" class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative">
-                Divisiones
-                <i class="fa-solid fa-sort-down text-sm"></i>
-                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
+              <a href="#"
+                class="items-center text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative flex">
+                <span data-i18n="divisions">Divisiones</span>
+                <i class="fa-solid fa-sort-down text-sm ml-1"></i>
+                <span
+                  class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
               </a>
-              <ul class="absolute top-full left-0 bg-white shadow-md rounded-md p-2 space-y-1 hidden group-hover:block w-52">
-                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500">CG</a></li>
-                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500">NCG</a></li>
-                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500">CHIH</a></li>
+              <ul
+                class="absolute top-full left-0 bg-white shadow-md rounded-md p-2 space-y-1 hidden group-hover:block w-52">
+                <li class="relative group-cg"> <a href="#"
+                    class="text-neutral-700 text-lg block py-1 hover:text-amber-500 flex justify-between items-center">
+                    <span data-i18n="logistics3pl">3PL Logistica</span>
+                    <i class="fa-solid fa-caret-right text-sm"></i>
+                  </a>
+                  <ul
+                    class="absolute top-0 left-full bg-white shadow-md rounded-r-md ml-2 space-y-1 hidden group-cg-hover:block w-80">
+                    <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                        data-i18n="douglasPort">REMS Port of Dougla and Arizona</a></li>
+                    <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                        data-i18n="pasoJuarez">Paso, TX / Juarez, Chih</a></li>
+                    <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                        data-i18n="hermosillo">Hermosillo, Sonora</a></li>
+                  </ul>
+                </li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="transport">Transporte</a></li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="railwayEngineering">Ingeniería ferroviaria</a></li>
+              </ul>
+            </li>
+            <li class="relative group">
+              <a href="#"
+                class="items-center text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative flex">
+                <span data-i18n="services">Servicios</span>
+                <i class="fa-solid fa-sort-down text-sm ml-1"></i>
+                <span
+                  class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
+              </a>
+              <ul
+                class="absolute top-full left-0 bg-white shadow-md rounded-md p-2 space-y-1 hidden group-hover:block w-72">
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="warehousing">Almacenamiento</a></li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="doortoDoor">Puerta-a-Puerta</a></li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="crossborder">Cruzar la frontera</a></li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="crossdockingandTransshipment">Muelle cruzado y Transbordo</a></li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="freightTransportation">Transporte de mercancías</a></li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="drayageandLastMileService">Servicio de Acarreo y Última Milla</a></li>
+                <li><a href="#" class="text-neutral-700 text-lg block py-1 hover:text-amber-500"
+                    data-i18n="roadTransportation">Transporte Terrestre</a></li>
               </ul>
             </li>
             <li>
-              <a href="#" class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
-                Servicios
-                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
+              <a href="#"
+                class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
+                <span data-i18n="clients">Clientes</span>
+                <span
+                  class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
               </a>
             </li>
             <li>
-              <a href="#" class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
-                Clientes
-                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
-                Contactos
-                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
+              <a href="#"
+                class="text-neutral-700 hover:text-amber-500 font-bold text-md md:text-sm lg:text-xl relative group">
+                <span data-i18n="contacts">Contactos</span>
+                <span
+                  class="absolute left-0 bottom-0 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full"></span>
               </a>
             </li>
           </ul>
         </nav>
       </div>
 
-      <div class="flex flex-shrink-0 items-center ml-4 sm:ml-6">
-        <router-link to="/auth/index" class="border-l border-gray-300 pl-3 ml-3 space-x-1">
-          <button to="admin/loj"
-            class="relative p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 rounded-full">
-            <span class="sr-only">Cerrar sesión</span>
-            <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-            </svg>
+      <div class="flex flex-shrink-0 items-center ml-4 md:ml-6">
+        <div class="flex sm:flex space-x-2 mr-3">
+          <button id="translate-es"
+            class="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-3 rounded-md focus:outline-none focus:shadow-outline"
+            @click="translateToSpanish">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvLgc7kJxsAj8OlJilbLLooCOCPA6Dh8GIMQ&s"
+              alt="Bandera de México" class="h-5 w-8">
           </button>
-        </router-link>
+          <button id="translate-en"
+            class="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-3 rounded-md focus:outline-none focus:shadow-outline"
+            @click="translateToEnglish">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1235px-Flag_of_the_United_States.svg.png"
+              alt="Bandera de USA" class="h-5 w-8">
+          </button>
+        </div>
       </div>
     </div>
 
@@ -136,6 +177,53 @@ export default {
   data() {
     return {
       url: process.env.VUE_APP_ROOT_ASSETS,
+      translations: {
+        es: {
+          contactUs: '¿Requieres consulta? Contactanos',
+          home: 'Inicio',
+          aboutUs: 'Sobre Nosotros',
+          divisions: 'Divisiones',
+          services: 'Servicios',
+          clients: 'Clientes',
+          contacts: 'Contactos',
+          logistics3pl: '3PL Logistica',
+          douglasPort: 'REMS Port of Dougla and Arizona',
+          pasoJuarez: 'Paso, TX / Juarez, Chih',
+          hermosillo: 'Hermosillo, Sonora',
+          transport: 'Transporte',
+          railwayEngineering: 'Ingeniería ferroviaria',
+          warehousing: 'Almacenamiento',
+          doortoDoor: 'Puerta-a-Puerta',
+          crossborder: 'Cruzar la frontera',
+          crossdockingandTransshipment: 'Muelle cruzado y Transbordo',
+          freightTransportation: 'Transporte de mercancías',
+          drayageandLastMileService: 'Servicio de Acarreo y Última Milla',
+          roadTransportation: 'Transporte Terrestre',
+        },
+        en: {
+          contactUs: 'Do you require a consultation? Contact us',
+          home: 'Home',
+          aboutUs: 'About Us',
+          divisions: 'Divisions',
+          services: 'Services',
+          clients: 'Clients',
+          contacts: 'Contacts',
+          logistics3pl: '3PL Logistics',
+          douglasPort: 'REMS Port of Douglas and Arizona',
+          pasoJuarez: 'El Paso, TX / Juarez, Chih',
+          hermosillo: 'Hermosillo, Sonora',
+          transport: 'Transportation',
+          railwayEngineering: 'Railway Engineering',
+          warehousing: 'Warehousing',
+          doortoDoor: 'Door-to-Door',
+          crossborder: 'Cross Border',
+          crossdockingandTransshipment: 'Cross docking and Transshipment',
+          freightTransportation: 'Freight Transportation',
+          drayageandLastMileService: 'Drayage and Last Mile Service',
+          roadTransportation: 'Road Transportation',
+        },
+      },
+      currentLanguage: 'es', // Idioma por defecto
     };
   },
   mounted() {
@@ -156,9 +244,28 @@ export default {
       });
     }
   },
+  methods: {
+    translateToSpanish() {
+      this.currentLanguage = 'es';
+      this.applyTranslations();
+    },
+    translateToEnglish() {
+      this.currentLanguage = 'en';
+      this.applyTranslations();
+    },
+    applyTranslations() {
+      const elements = document.querySelectorAll('[data-i18n]');
+      elements.forEach((el) => {
+        const key = el.getAttribute('data-i18n');
+        if (this.translations[this.currentLanguage] && this.translations[this.currentLanguage][key]) {
+          el.textContent = this.translations[this.currentLanguage][key];
+        }
+      });
+    },
+  },
   setup() {
     const store = useStore();
-    const isMobile = ref(window.innerWidth < 768); // Define el breakpoint para móvil
+    const isMobile = ref(window.innerWidth < 768);
 
     const handleResize = () => {
       isMobile.value = window.innerWidth < 768;
@@ -188,7 +295,6 @@ export default {
 .bgHeader1 {
   background-color: rgb(254 152 0);
   border-bottom: 1px solid #e2e8f0;
-  /* Añade una línea sutil en la parte inferior */
 }
 
 .bgHeader2 {
@@ -198,6 +304,9 @@ export default {
 .bgHeader {
   background-color: white;
   border-bottom: 1px solid #e2e8f0;
-  /* Añade una línea sutil en la parte inferior */
+}
+
+.group-cg:hover .group-cg-hover\:block {
+  display: block;
 }
 </style>
